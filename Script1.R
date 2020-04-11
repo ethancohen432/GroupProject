0fd37fc5e8db49c93b3c947f0e06659dccd13a4e
GabrielleMcCabe <15gamm@queensu.ca>
2020-04-11 11:30:22 -0400

    Updated script1.

--- Script1.R
+++ Script1.R
@@ -2,3 +2,6 @@ library(seqinr)
 dna <- read.fasta("data/raw_seqs_BB.fna")
 
 OTU_table <- read.delim("data/OTU_file.txt", header = T)
+
+
+getSequence(dna$B1285_1)  # How to get just the sequence of one read from one person. 
